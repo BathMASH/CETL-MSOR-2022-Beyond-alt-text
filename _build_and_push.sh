46aa9123aa4447d3d3d789d001e218c2d680c314
Ed Southwood <edsouthwood@gmail.com>
2022-08-03 14:09:49 +0100

    Update the book

--- _build_and_push.sh
+++ _build_and_push.sh
@@ -2,6 +2,6 @@
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
-#git add .
-#git commit -m "Update the book"
-#git push origin master
+git add .
+git commit -m "Update the book"
+git push origin main
